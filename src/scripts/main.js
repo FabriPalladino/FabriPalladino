fed0f395cb999f8c780a7bda3f31428ad62f5b23
FabriPalladino <palladino.fabrizio@gmail.com>
2023-08-24 15:14:29 +0200

    Typography

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,10 @@
 import { removeHyphens } from './utils';
+import { fontObserver } from './fonts-manager';
 
 class App {
   init () {
     console.log(removeHyphens('H-e-l-l-o- -w-o-r-l-d-!'));
+    fontObserver();
   }
 }
 
